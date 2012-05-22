3571ac97f69b86200c763ff35816467ad0137d10
rivendell <nishac92@gmail.com>
2012-05-22 10:58:24 +0530

    corrected main.m. Hope this is not as absurd as the previous commit.

--- main.m
+++ main.m
@@ -1,70 +1,106 @@
-function U =  main(nu,xmin,xmax,N)
-  c=1;
-  h=(xmax-xmin)/(N+1);
-  dt=nu*h/c;
-  [U0 Du0 D2u0]= plotInitial(xmin, xmax, N);
-  U = getU(U0, Du0, D2u0,h,dt,N,nu);
-  x = xmin:h:xmax;
-  f = figure;
-  set(f, "visible", "off");
-  #for i = 1:2*(N+2)
-     #plot(x, U(i:(N+i+1)));
-     #hold on;
-     #i= i + N + 2;
-  #endfor;
-  plot(x, U(1:(N+2)));
-  print("plots.png", "-dpng");  
+function main(nu, xmin, xmax, N)
+	h = (xmax-xmin)/(N+1);
+	c=1;
+	Nt = 25;
+	dt = h*nu/c;
+	x = xmin:h:xmax;
+        ch = input("1. continuous 2.discontinuous ");
+        if(ch==1)
+                u0=plotInitialCont(xmin,xmax,N);
+        elseif(ch==2)
+                u0=plotInitialDis(xmin,xmax,N);
+        else
+                disp("wrong choice!");
+        endif;
+	choice = input("1. forward 2. backward 3. central 4. lax");
+	if(choice==1)
+		forward(x,u0,nu,N,Nt);
+	elseif(choice==2)
+		backward(x,u0,nu,N,Nt);
+	elseif(choice==3)
+		central(x,u0,nu,N,Nt);
+	elseif(choice==4)
+		lax(x,u0,nu,N,Nt);
+	else
+		disp("wrong choice!");
+	endif;
 endfunction;
-function P = getU(u0, du0 ,d2u0,h,dt,N,nu)
-    P = [];
-    for i = 1:2
-      P = [P ; Un(i, u0, du0, d2u0, h, dt, N, nu)];
-    endfor;
-endfunction
-function un = Un(n,u0, du0, d2u0, h, dt, N, nu)
-    un=[];
-    for j = 1:N+2
-      un = [ un, u(j,n,u0,du0, d2u0, h, dt, N ,nu)];
-    endfor;
+function forward(x,u0,nu,N,Nt)
+	unp1 = u0;
+        f = figure;
+        set(f,"visible","off");
+	for n=2:Nt
+		un= unp1;
+		for j=1:N+1
+			unp1(j) = (1+nu)*un(j) - nu*un(j+1);
+		endfor;
+		unp1(N+2)=0;
+		if(mod(n,5)==0)
+			plot(x,unp1,'color',n/5+1);
+			hold on;
+		endif;
+	endfor;
+	print("fplots.png", "-dpng");
 endfunction;
-function ujn = u(j,n,u0, du0, d2u0, h, dt, N, nu)
-    if(n==1)
-      ujn= u0(j);
-    else
-      ujn = u(j,n-1,u0,du0,d2u0,h,dt,N,nu)*(1+nu) - nu*(u(j,n-1,u0,du0,d2u0,h,dt,N,nu) + h*du(n-1,u0,du0,d2u0,h,dt,N,nu)(j) + h*h/2*d2u(n-1,u0,du0,d2u0,h,dt,N,nu)(j));
-    endif
+function backward(x,u0,nu,N,Nt)
+	unm1 = u0;
+        f=figure;
+	set(f, "visible", "off");
+	for n=2:Nt
+		un= unm1;
+		unm1(1)=0;
+		for j=2:N+2
+			unm1(j) = (1+nu)*un(j) - nu*un(j-1);
+		endfor;
+		if(mod(n,5)==0)
+    			plot(x,unm1);
+			hold on;
+		endif;
+	endfor;
+	print("bplots.png", "-dpng");
 endfunction;
-function approx = du(n,u0,du0,d2u0,h,dt,N,nu)
-    if(n==1)
-      approx= du0;
-    else
-      approx1 = [diff(u(n,u0,du0,d2u0,h,dt,N,nu)),0.00];
-      approx = approx1/h;
-    endif;
+function central(x,u0,nu,N,Nt)
+	un = u0;
+	unm1=u0;
+	unp1=u0;
+	f=figure;
+	set(f, "visible", "off");
+	for n=4:Nt
+		unm1=un;
+		un=unp1;
+		for j=2:N+1
+			unp1(j) = unm1(j) - nu*un(j+1) + nu*un(j-1);
+		endfor;
+		unp1(1)=0;
+		unp1(N+2)=0;
+		if(mod(n,5)==0)
+			plot(x, unp1);
+                        hold on;
+		endif;
+	endfor;
+	print("cplots.png", "-dpng");
 endfunction;
-function approx = d2u(n,u0,du0,d2u0,h,dt,N,nu)
-    if(n==1)
-      approx = d2u0;
-    else
-      approx1 = [diff(du(n,u0,du0,d2u0,h,dt,N,nu)), 0.000];
-      approx = approx1/h;
-    endif;
-endfunction;
- 
-  
-  #for n= 2:5
-     #U = [U ; u(n)];
-  #endfor;
+function lax(x,u0,nu,N,Nt)
+	unp1=u0;
+        f = figure;
+        set(f,"visible","off");
+      	for n=2:Nt
+		un=unp1;
+		unp1(1)=0;
+		unp1(N+2)=0;
+		for j=2:N+1
+			unp1(j) = un(j) - nu*(un(j+1)-un(j-1))/2 + nu*nu/2*(un(j-1)-2*un(j)+un(j+1));
+		endfor;
+		if(mod(n,5)==0)
+			plot(x,unp1);
+                        hold on;
+		endif;
+	endfor;
+	print("lplots.png","-dpng");
+endfunction;	
 
-    
-  
-  
-  
-
-  
-  
-  
+		
 
 
-
-
+		
+	
